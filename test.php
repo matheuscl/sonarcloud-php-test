ac7d9157e5e52287d227e7db976c7b5f7b8df2e7
Matheus Lopes <matheus1994lopes@gmail.com>
2022-03-23 18:36:49 -0300

    Initial commit

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+class Foo {
+    private function bar($param1, $param2, $param3, $param4,, $param5, $param6, $param7) {
+        echo 'Not too many params =/';
+    }
+
+    private function baz($param1, $param2, $param3, $param4,, $param5, $param6, $param7, $param8, $param9, $param10) {
+        echo 'Too many params =/';
+    }
+}
